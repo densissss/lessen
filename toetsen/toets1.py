d28136e020a30cdcfa6b8c265fd2344b152b524b
densissss <dennis.versyck@gmail.com>
2023-12-14 20:32:20 +0100

    toets

---
+++ toetsen/toets1.py
@@ -0,0 +1,98 @@
+#  _____       _   _                     _             _         __ 
+# |  __ \     | | | |                _  | |           | |       /_ |
+# | |__) |   _| |_| |__   ___  _ __ (_) | |_ ___   ___| |_ ___   | |
+# |  ___/ | | | __| '_ \ / _ \| '_ \    | __/ _ \ / _ \ __/ __|  | |
+# | |   | |_| | |_| | | | (_) | | | |_  | || (_) |  __/ |_\__ \  | |
+# |_|    \__, |\__|_| |_|\___/|_| |_(_)  \__\___/ \___|\__|___/  |_|
+#         __/ |                                                     
+#        |___/                                                      
+# =====================================================================                                  
+
+
+# In deze oefening wil ik bekijken of je de basis van python onder de knie hebt
+# Volg de opdrachten nauwgezet op. Laat je script ook af en toe afspelen om te kijken of alles goed werkt!
+
+# ************************************************************************************************************
+# Begin alvast door je naam in te vullen in de variabale hieronder. Zet je naam tussen de "":
+# ************************************************************************************************************
+
+naam = "mijnNaam"
+
+# als je je script een eerste keer uitvoert kan je controleren of de eerste opdracht goed is gelukt.
+print("Hallo,", naam + ". Als hier je eigen naam staat is de eerste opdracht alvast geslaagd!")
+print("**************************************************************************************")
+
+# In deze eerste oefening is het de bedoeling dat je een bepaalde tekst kunt "printen" op je scherm
+# ! tip: gebruik hiervoor de print()-funtie
+# ! tip2: vergeet niet dat tekst die je wil printen tussen "" moet staan!
+
+# We beginnen er aan. print alles wat tussen de sterretjes staat: 
+# Gebruik voor elke lijn een nieuwe print()-opdracht!
+
+# ************************************************************************************************************
+# Hallo,
+# Ik kan al een beetje programmeren.
+# Ik moet wel nog veel leren!
+# ************************************************************************************************************
+
+
+
+
+
+
+#laat de lijn hieronder staan:
+print("**************************************************************************************")
+
+# nu gaan we een variabele (= een doos) maken en hier een tekst in opslaan
+# bvb: doos = "leeg"
+
+# ************************************************************************************************************
+# maak nu de variabele geboorteland en vul je geboorteland hier in (vergeet de "" niet!)
+# ************************************************************************************************************
+
+
+
+# ************************************************************************************************************
+# Als je je programma uitvoert gebeurt er niets. We moeten je variabele dus nog printen!
+# ************************************************************************************************************
+
+
+
+# BOOYA!
+# Als alles goed is heb je nu je geboorteland geprint, door de variabele te printen (en niet de string!).
+
+#laat de lijn hieronder staan:
+print("**************************************************************************************")
+
+#We gaan verder oefenen in het maken van variabelen:
+
+# ************************************************************************************************************
+# Maak 3 variabelen aan:
+# 1. favorieteFilm
+# 2. favorieteVak
+# 3. favorieteEten
+# geef ze ook een waarde naar keuze
+# print al deze waarden op de juiste plaats:
+# ************************************************************************************************************
+
+print("Mijn favoriete film is:")
+# print hier de variabele favorieteVak
+
+print("Mijn favoriete vak in de schakelklas is:")
+#print hier de variabele favorieteVak
+
+print("Mijn favoriete eten is:")
+#print hier de variabele favorieteEten
+
+
+# ************************************************************************************************************
+# Laatste oefening: Maak een mooie zin door de variabelen goed in te vullen:
+# ************************************************************************************************************
+
+onderwerp = ""
+persoonsvorm = ""
+adjectief = ""
+
+print("De", onderwerp, persoonsvorm, adjectief + ".")
+
+
